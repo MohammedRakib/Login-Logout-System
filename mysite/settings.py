70fd59468b3984e4a2540c2f9e5a8d760e60d71f
Mohammed Rakib <rafz.rakib@gmail.com>
2020-07-14 03:25:45 +0600

    lol

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
 }
 import dj_database_url
 
-lol = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(lol)
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
